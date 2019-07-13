19a7c2450780c6b3f941982cb5fa41acfe0e9423
ScooterOk <scooterok@gmail.com>
2019-07-13 16:37:00 +0300

    documents

--- build/js/app.js
+++ build/js/app.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
+  // Sliders
   if($(".slider").length){
     $(".slider").slick();
   }
 
+  // Form 
   $('.form-control input, .form-control textarea').focus(function(e){
     $(this).closest('.form-control').addClass('focus');
   });
@@ -12,10 +14,8 @@ $(document).ready(function(){
       $(this).closest('.form-control').removeClass('focus');
     }    
   });
-
   $('.form-select select').change(function(e){
     $(this).closest('.form-select').addClass('selected');
   });
-
   $('#phone-mask').mask('000-000-0000');
 });
